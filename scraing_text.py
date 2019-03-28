3d6b9d3fac6b23f28e8a6316b352e58c57e924bb
BaiChen <baichen318@gmail.com>
2019-03-28 21:02:00 +0800

    A smart car controlled by Raspberry Pi, can recognize images through Tensorflow The car is controlled by Raspberry Pi 3B, with a capability of recognizing images captured through a camera.

---
+++ scraing_text.py
@@ -0,0 +1,9 @@
+# Get the result with the highest possibility
+
+import re
+
+
+aim=re.compile(r'([A-Za-z]+\s*[A-Za-z]*)',re.S)
+with open('result.txt','r') as f:
+    result=re.findall(aim,f.read())
+    print(result[0])
